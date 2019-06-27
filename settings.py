a833f94006064e9744e59e489c31ecfc2406494e
Mark Polyak <markpolyak@users.noreply.github.com>
2019-06-27 19:26:30 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,18 @@
+# TODO: put your GitHub token here
+github_token="1234567890abcdef" 
+github_organization = "suai-os-2019"
+
+# TODO: put your Appveyor token here
+appveyor_token = "1234567890abcdef" 
+# TODO: put your Appveyor username here
+appveyor_account = "username" 
+
+# TODO: put your Travis CI token here
+travis_token = "1234567890abcdef" 
+
+# TODO: make sure this file exists and has a valid Google API private key
+gsheet_key_filename = "SUAI-OS-2019-ee72bf6e6075.json" 
+gspreadsheet_name = "Операционные системы 2019 весна"
+
+# номер лабораторной работы и количество вариантов
+os_assignment_tasks = {1: 12, 2: 20, 3: 20}
